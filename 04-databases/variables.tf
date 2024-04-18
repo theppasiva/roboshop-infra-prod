83f7ffa653892f76f0975f04025f7e286becc560
theppasiva <theppasiva@gmail.com>
2024-04-18 22:33:24 +0530

    roboshop-infra-dev

---
+++ 04-databases/variables.tf
@@ -0,0 +1,19 @@
+variable "common_tags" {
+    default = {
+        Project = "roboshop"
+        Environment = "dev"
+        Terraform = "true"
+    }
+}
+
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_name" {
+  default = "shivarampractise.online"
+}
