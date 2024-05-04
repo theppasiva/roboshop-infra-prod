4420b78442bd6eaf175d640431abd581c46e091d
theppasiva <theppasiva@gmail.com>
2024-05-04 15:56:38 +0530

    added shipping and cart module

---
+++ 12-shipping/variables.tf
@@ -0,0 +1,29 @@
+variable "common_tags" {
+    default = {
+        Project = "roboshop"
+        Environment = "dev"
+        Terraform = "true"
+    }
+}
+
+variable "tags" {
+ default = {
+    Component = "shipping"
+ }
+}
+
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_name" {
+  default = "shivarampractise.online"
+}
+
+variable "iam_instance_profile" {
+  default = "Ec2roleForShellSript"
+}
